df5f9a5788e6f16ebd55f5db392ce0c62515d485
Yuki <yukithm@gmail.com>
2018-10-11 10:27:41 +0900

    Read files from arguments

--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"io"
 	"os"
 
 	"golang.org/x/text/transform"
 )
 
 func main() {
-	msg, err := DecodeMail(os.Stdin)
-	if err != nil {
-		panic(err)
-	}
-
 	normalizer := &NewlineNormalizer{
 		Newline: []byte("\r\n"),
 	}
 	w := transform.NewWriter(os.Stdout, normalizer)
 	defer w.Close()
+
+	if len(os.Args) > 1 {
+		for _, file := range os.Args[1:] {
+			printFile(w, file)
+		}
+	} else {
+		printMail(w, os.Stdin)
+	}
+
+}
+
+func printFile(w io.Writer, file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return printMail(w, f)
+}
+
+func printMail(w io.Writer, f io.Reader) error {
+	msg, err := DecodeMail(f)
+	if err != nil {
+		return err
+	}
 	PrintMessage(w, msg)
+	return nil
 }
